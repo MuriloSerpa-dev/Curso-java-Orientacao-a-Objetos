927baec31bef55016209b03c96f4f962d8ef08bf
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-04-28 15:40:31 -0300

    3.14 Exercicio objetos e metodos

---
+++ src/Passeio.java
@@ -0,0 +1,18 @@
+public class Passeio {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Andre";
+
+        pessoa.cachorro = new Cachorro();
+
+        pessoa.cachorro.nome = " Ralf";
+        pessoa.cachorro.idade = 5;
+        pessoa.cachorro.raca = " Pug";
+        pessoa.cachorro.sexo = "M";
+
+        Caminhada caminhar = new Caminhada();
+
+        caminhar.andar(pessoa);
+
+    }
+}
