27517442c0180897624ab3b37509c375b91d751f
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-05-04 12:50:54 -0300

    5.9 Modificador de acesso default

---
+++ src/com/algaworksfjo/treinador/DonoCachorro.java
@@ -0,0 +1,11 @@
+package com.algaworksfjo.treinador;
+
+
+import com.algaworksfjo.animal.Cachorro;
+
+public class DonoCachorro {
+
+    void ensinarACachorroSentar(Cachorro cachorro){
+        cachorro.sentar();
+    }
+}
