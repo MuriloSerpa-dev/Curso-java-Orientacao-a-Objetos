fb3750b527b4e5e8d26a413d51db59923d42d50f
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-05-04 12:04:51 -0300

    5.8 Organizacao de pacotes

---
+++ src/com/algaworksfjo/treinador/TreinadorCachorro.java
@@ -0,0 +1,14 @@
+package com.algaworksfjo.treinador;
+
+import com.algaworksfjo.animal.Cachorro;
+
+public class TreinadorCachorro {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+
+        cachorro.setNome("Bobscuite");
+
+        System.out.println(cachorro.getNome());
+
+    }
+}
