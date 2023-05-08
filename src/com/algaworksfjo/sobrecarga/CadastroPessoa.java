053d855df56b332657a066f301a407022055d91d
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-05-08 17:30:00 -0300

    5.18 Sobrecarga

---
+++ src/com/algaworksfjo/sobrecarga/CadastroPessoa.java
@@ -0,0 +1,15 @@
+package com.algaworksfjo.sobrecarga;
+
+public class CadastroPessoa {
+    public void cadastrar (Pessoa pessoa){
+        armazenar(pessoa.getNome(), pessoa.getIdade());
+    }
+
+    public void cadastrar(String nome , int idade){
+        armazenar(nome , idade);
+
+    }
+    private void armazenar(String nome, int idade){
+        System.out.println("Salvando: \"" + nome + "\" com " + idade + " anos.");
+    }
+}
---
+++ src/com/algaworksfjo/sobrecarga/TesteCadastro.java
@@ -0,0 +1,16 @@
+package com.algaworksfjo.sobrecarga;
+
+public class TesteCadastro {
+    public static void main(String[] args) {
+    CadastroPessoa cadastro = new CadastroPessoa();
+    Pessoa pessoa = new Pessoa("Murilo", 28);
+
+    cadastro.cadastrar(pessoa);
+    cadastro.cadastrar("Fernando", 35);
+
+
+
+
+
+    }
+}
