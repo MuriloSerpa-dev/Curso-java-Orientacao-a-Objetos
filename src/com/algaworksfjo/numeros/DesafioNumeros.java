8c77dc4ccf30e7c754257eeb32881d25ec3bcc9d
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-05-19 12:08:43 -0300

    6.10 Desafio : Numeros

---
+++ src/com/algaworksfjo/numeros/DesafioNumeros.java
@@ -0,0 +1,26 @@
+package com.algaworksfjo.numeros;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class DesafioNumeros {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("R$ #,##0.00");
+
+        System.out.println("Digite valor produto: ");
+        double valorProduto = entrada.nextDouble();
+
+
+        BigDecimal bg = new BigDecimal(valorProduto);
+        BigDecimal resultado = bg.multiply(BigDecimal.TEN).divide(new BigDecimal(100));
+
+
+        String valorFormatado = formatador.format(resultado.doubleValue());
+
+        System.out.println("10% do valor é " + valorFormatado );
+
+
+    }
+}
